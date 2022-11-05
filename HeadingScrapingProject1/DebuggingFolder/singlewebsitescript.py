15a37c45826f89cc5ea4e8a5d37401134242cf5a
Your Name <you@example.com>
2022-11-05 18:19:18 +0500

    second

--- HeadingScrapingProject1/DebuggingFolder/singlewebsitescript.py
+++ HeadingScrapingProject1/DebuggingFolder/singlewebsitescript.py
@@ -7,34 +7,24 @@ options.add_argument("start-maximized")
 options.add_argument("--log-level=3")
 options.add_argument("--headless")
 driver = webdriver.Chrome(options=options)
-driver.get('https://www.visaplace.com/blog-immigration-law/c/canada/')
+driver.get("https://www.mygreatlearning.com/blog/top-highest-paying-jobs-in-the-world/")
 page=driver.page_source
 soup=BeautifulSoup(str(page),'html.parser')
-bodyTag=soup.body
-print(bodyTag.prettify())
-bodyTag.header.decompose()
-bodyTag.footer.decompose()
-bodyTag.aside.decompose()
+bodyTag=soup.body.main
+#print(bodyTag.prettify())
 
-header=bodyTag.header
-print("header",header)
-footer=bodyTag.footer
-print("footer",footer)
-aside=bodyTag.aside
-print("aside",aside)
-print("body updated",bodyTag.prettify())
+if (bodyTag):
 
-h3s=bodyTag.find_all('h3')
-h2s=bodyTag.find_all('h2')
-h1s=bodyTag.find_all('h1')
+	h3s=bodyTag.find_all('h3')
+	h2s=bodyTag.find_all('h2')
+	h1s=bodyTag.find_all('h1')
 
-for h3 in h3s:
-	
-    print(h3.text)
-print("-----------------------------")
-for h2 in h2s:
-	print(h2.text)
-print("-----------------------------")
-for h1 in h1s:
-	print(h1.text)
-print("-----------------------------")
+	for h3 in h3s:
+		print(h3.text)
+	print("-----------------------------")
+	for h2 in h2s:
+		print(h2.text)
+	print("-----------------------------")
+	for h1 in h1s:
+		print(h1.text)
+	print("-----------------------------")
--- HeadingScrapingProject1/DebuggingFolder/tempCodeRunnerFile.py
+++ HeadingScrapingProject1/DebuggingFolder/tempCodeRunnerFile.py
@@ -1,2 +1,14 @@
+	h3s=bodyTag.find_all('h3')
+	h2s=bodyTag.find_all('h2')
+	h1s=bodyTag.find_all('h1')
 
-driver = webdriver.Chrome(options=options)
+	for h3 in h3s:
+		
+		print(h3.text)
+	print("-----------------------------")
+	for h2 in h2s:
+		print(h2.text)
+	print("-----------------------------")
+	for h1 in h1s:
+		print(h1.text)
+	print("-----------------------------")
